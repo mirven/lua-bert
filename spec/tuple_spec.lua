2ace8a4b725d9c28a08cd17a7a7a15c994af01d4
Marcus Irven <marcus@marcusirven.com>
2009-11-22 22:07:56 -0600

    require tuple into local variable for tuple specs

--- spec/tuple_spec.lua
+++ spec/tuple_spec.lua
@@ -1,12 +1,12 @@
 require 'luaspec'
-require 'bert.tuple'
+local tuple = require 'bert.tuple'
 
 describe["tuples"] = function()
 	before = function()
-		tuple = bert.tuple.t { 1,2,3 }
+		t = tuple.t { 1,2,3 }
 	end
 	
 	it["should be able to check that it is a tuple"] = function()
-		expect(bert.tuple.is_tuple(tuple)).should_be(true)
+		expect(tuple.is_tuple(t)).should_be(true)
 	end
 end
