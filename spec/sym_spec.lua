d104238059237d1142cf7e30092d200ae95fe6c0
Marcus Irven <marcus@marcusirven.com>
2009-11-22 22:08:58 -0600

    require sym into local variable for symbol tests

--- spec/sym_spec.lua
+++ spec/sym_spec.lua
@@ -1,29 +1,29 @@
 require 'luaspec'
-require 'bert.sym'
+local sym = require 'bert.sym'
 
 describe["symbols"] = function()
 	it["should create the same symbol from the same string"] = function()
-		expect(bert.sym.sym("foo")).should_be(bert.sym.sym("foo"))
+		expect(sym.sym("foo")).should_be(sym.sym("foo"))
 	end
 
 	it["should create different symbols from different strings"] = function()
-		expect(bert.sym.sym("foo")).should_not_be(bert.sym.sym("bar"))
+		expect(sym.sym("foo")).should_not_be(sym.sym("bar"))
 	end
 	
 	it["should be able to provide it's name"] = function()
-		expect(bert.sym.sym("foo").name).should_be("foo")
+		expect(sym.sym("foo").name).should_be("foo")
 	end
 	
 	describe["checking to see if an object is a symbol"] = function()
 		it["should return true when passed a symbol"] = function()
-			expect(bert.sym.is_symbol(bert.sym.sym("foo"))).should_be(true)
+			expect(sym.is_symbol(sym.sym("foo"))).should_be(true)
 		end
 		
 		it["should return false when passed anything else"] = function()
-			expect(bert.sym.is_symbol({})).should_be(false)
-			expect(bert.sym.is_symbol()).should_be(false)
-			expect(bert.sym.is_symbol(true)).should_be(false)
-			expect(bert.sym.is_symbol("foo")).should_be(false)
+			expect(sym.is_symbol({})).should_be(false)
+			expect(sym.is_symbol()).should_be(false)
+			expect(sym.is_symbol(true)).should_be(false)
+			expect(sym.is_symbol("foo")).should_be(false)
 		end
 	end
 	
