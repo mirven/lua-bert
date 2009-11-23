a69a26bb75360b769fe45e71243eb4ad2b4d6eed
Marcus Irven <marcus@marcusirven.com>
2009-11-23 00:37:36 -0600

    add test for tuple comparison

--- spec/tuple_spec.lua
+++ spec/tuple_spec.lua
@@ -9,4 +9,9 @@ describe["tuples"] = function()
 	it["should be able to check that it is a tuple"] = function()
 		expect(tuple.is_tuple(t)).should_be(true)
 	end
+	
+	it["should be able to compared against other tuples for equality"] = function()
+		expect(t).should_be(tuple.t{ 1,2,3 })
+		expect(t).should_not_be(tuple.t{ 1,2,3,4 })
+	end
 end
