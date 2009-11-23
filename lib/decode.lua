5f68aee623b9902170edc7daaf7d317806f412e6
Marcus Irven <marcus@marcusirven.com>
2009-11-22 18:43:57 -0800

    Add decoding

---
+++ lib/decode.lua
@@ -0,0 +1,186 @@
+require 'types'
+require 'sym'
+require 'tuple'
+
+Decoder = {}
+Decoder.__index = Decoder
+
+function Decoder:new(str)
+	local input = {}
+	
+	for i=1,str:len() do
+		input[#input+1] = str:byte(i)
+	end
+	return setmetatable({ input = input, current = 1 }, self)
+end
+
+function Decoder:read_any()
+	if self:read_1() ~= Types.MAGIC then
+		error "Bad Magic"
+	end
+	return self:read_any_raw()
+end
+
+function Decoder:read_any_raw()
+	local next_byte = self:peek_1()
+	if next_byte == Types.ATOM then return self:read_atom()
+	elseif next_byte == Types.SMALL_INT then return self:read_small_int()
+	elseif next_byte == Types.INT then return self:read_int()
+	elseif next_byte == Types.SMALL_BIGNUM then return self:read_small_bignum()
+	elseif next_byte == Types.LAGE_BIGNUM then return self:read_large_bignum()
+	elseif next_byte == Types.FLOAT then return self:read_float()
+	elseif next_byte == Types.SMALL_TUPLE then return self:read_small_tuple()
+	elseif next_byte == Types.LARGE_TUPLE then return self:read_large_tuple()
+	elseif next_byte == Types.NIL then return self:read_nil()
+	elseif next_byte == Types.STRING then return self:read_string()
+	elseif next_byte == Types.LIST then return self:read_list()
+	elseif next_byte == Types.BIN then return self:read_bin()
+	else
+		error("Unknown type: "..tostring(next_byte).." at "..tostring(self.current))
+	end
+end
+
+function Decoder:peek(length)
+	local bytes = {}
+	
+	local start_index = self.current
+	local end_index = self.current+length-1
+	
+	for i=start_index, end_index do
+		bytes[#bytes+1] = self.input[i]
+	end
+	return bytes
+end
+
+function Decoder:read(length)	
+	local bytes = self:peek(length)
+	self.current = self.current + length
+	return bytes
+end
+
+function Decoder:peek_1()
+	return self:peek(1)[1]
+end
+
+function Decoder:read_1()
+	return self:read(1)[1]
+end
+
+function Decoder:read_2()
+	local bytes = self:read(2)
+	return bytes[2] -- TODO
+end
+
+function Decoder:read_4()
+	local bytes = self:read(4)
+	return bytes[4] -- TODO
+end
+
+function Decoder:read_string(length)
+	return string.char(unpack(self:read(length)))
+end
+
+function Decoder:read_atom()
+	if self:read_1() ~= Types.ATOM then error("Invalid Type, not an atom") end
+	local length = self:read_2()
+	local name = self:read_string(length)
+	return sym(name)
+end
+
+function Decoder:read_small_int()
+	if self:read_1() ~= Types.SMALL_INT then error("Invalid Type, not a small int") end
+	return self:read_1()
+end
+
+
+function Decoder:read_bin()
+	if self:read_1() ~= Types.BIN then error("Invalid Type, not an erlang binary") end
+  local length = self:read_4()
+  return self:read_string(length)
+end
+
+function Decoder:read_dict()
+	local type = self:read_1()
+	if type ~= Types.LIST or type ~= Types.NIL then error "Invalid dict spec, not an erlang list" end
+	
+	local length
+	if type == Types.LIST then
+		length = self:read_4()
+	else
+		length = 0
+	end
+	
+	local hash = {}
+	
+	for i=1,length do
+		local pair = self:read_any_raw()
+		hash[pair[0]] = pair[1]
+	end
+	if type == Types.LIST then
+		self:read_1()
+	end
+	return hash	
+end
+
+
+function Decoder:read_complex_type(arity)
+	local item = self:read_any_raw()
+	if item == sym "nil" then
+		return nil
+	elseif item == sym "true" then
+		return true
+	elseif item == sym "false" then
+		return false
+	elseif item == sym "time" then
+  	-- Time.at(read_any_raw * 1_000_000 + read_any_raw, read_any_raw)
+		error "Not Yet Implemented"
+	elseif item == sym "regex" then
+		error "Not Yet Implemented"
+	elseif item == sym "dict" then
+		return self:read_dict()
+	else
+		return nil
+	end	
+end
+
+function Decoder:read_tuple(arity)
+	if arity > 0 then
+		local tag = self:read_any_raw()
+		if tag == sym "bert" then
+			return self:read_complex_type(arity)
+		else
+			local tuple = t {}
+			tuple[1] = tag
+			for i=1,arity-1 do
+				tuple[i+1] = self:read_any_raw()
+			end
+			return tuple
+		end
+	else
+		error "Not Yet Implemented"
+		-- Tuple.new
+	end
+end
+
+function Decoder:read_list()
+	if self:read_1() ~= Types.LIST then error("Invalid Type, not an erlang list") end
+	local length = self:read_4()
+	local list = {}
+	for i=1,length do
+		list[i] = self:read_any_raw()
+	end
+	self:read_1()
+	return list
+end
+
+function Decoder:read_small_tuple()
+	if self:read_1() ~= Types.SMALL_TUPLE then error("Invalid Type, not a small tuple") end
+	local size = self:read_1()
+	return self:read_tuple(size)
+end
+
+function Decoder:read_large_tuple()
+	if self:read_1() ~= Types.LARGE_TUPLE then error("Invalid Type, not a large tuple") end
+	local size = self:read_4()
+	return self:read_tuple(size)
+end
---
+++ spec/decode_spec.lua
@@ -0,0 +1,162 @@
+require 'luaspec'
+require 'decode'
+
+matchers.should_fail = matchers.should_error
+
+function bytes_to_string(bytes)
+	return string.char(unpack(bytes))
+end
+
+describe["bert decoding"] = function()
+	describe["peeking"] = function()
+		before = function()
+			decoder = Decoder:new("ABCDEF")  -- 65,66,67,68,69
+		end
+		
+		it["should return the correct number of bytes"] = function()
+			local bytes = decoder:peek(2)
+			expect(#bytes).should_be(2)
+			expect(bytes[1]).should_be(65)
+			expect(bytes[2]).should_be(66)
+		end
+		
+		it["should not affect the current pointer"] = function()
+			expect(decoder.current).should_be(1)
+			decoder:peek(2)
+			expect(decoder.current).should_be(1)
+		end
+		
+		describe["when current is not at the start"] = function()
+			before = function()
+				decoder.current = 3
+			end
+			
+			it["should return bytes starting at the current pointer"] = function()
+				local bytes = decoder:peek(2)
+				expect(#bytes).should_be(2)
+				expect(bytes[1]).should_be(67)
+				expect(bytes[2]).should_be(68)			
+			end
+		end		
+		
+		describe["peeking at 1 byte"] = function()
+			before = function()
+				byte = decoder:peek_1()
+			end
+		
+			it["should return the byte at the current pointer"] = function()
+				expect(byte).should_be(65)
+			end
+			
+			it["should not affect the current pointer"] = function()
+				expect(decoder.current).should_be(1)
+			end
+		end
+	end
+	
+	describe["reading"] = function()
+		before = function()
+			decoder = Decoder:new("ABCDEF")  -- 65,66,67,68,69
+		end
+	
+		it["should return the correct number of bytes"] = function()
+			local bytes = decoder:read(2)
+			expect(#bytes).should_be(2)
+			expect(bytes[1]).should_be(65)
+			expect(bytes[2]).should_be(66)
+		end
+		
+		it["should update the current pointer"] = function()
+			expect(decoder.current).should_be(1)
+			decoder:read(2)
+			expect(decoder.current).should_be(3)
+		end
+		
+		describe["reading at 1 byte"] = function()
+			before = function()
+				byte = decoder:read_1()
+			end
+		
+			it["should return the byte at the current pointer"] = function()
+				expect(byte).should_be(65)
+			end
+			
+			it["should update the current pointer"] = function()
+				expect(decoder.current).should_be(2)
+			end
+		end	
+	end
+	
+	describe["decoding"] = function()
+		it["should fail if the first byte is not the proper magic"] = function()
+			decoder = Decoder:new("ABCDEF")  -- 65,66,67,68,69
+			expect(function() decoder:read_any() end).should_fail()
+		end		
+		
+		describe["decoding strings"] = function()
+			before = function()
+				decoder = Decoder:new(bytes_to_string { 131, 109, 0, 0, 0, 3, 102, 111, 111 })
+			end
+			
+			it["should return the encoded string"] = function()
+				expect(decoder:read_any()).should_be("foo")
+			end
+		end
+		
+		describe["decoding symbols"] = function()
+			before = function()
+				decoder = Decoder:new(bytes_to_string { 131, 100, 0, 3, 102, 111, 111 })
+			end
+		
+			it["should return the encoded symbol"] = function()
+				expect(decoder:read_any()).should_be(sym "foo")
+			end
+		end
+		
+		describe["decoding tuples"] = function()
+			before = function()
+				decoder = Decoder:new(bytes_to_string { 131, 104, 3, 109, 0, 0, 0, 1, 97, 109, 0, 0, 0, 1, 98, 109, 0, 0, 0, 1, 99 })
+			end
+			
+			it["should return encoded tuple"] = function()
+				local tuple = decoder:read_any()
+				expect(is_tuple(tuple)).should_be(true)
+				expect(#tuple).should_be(3)
+				expect(tuple[1]).should_be('a')
+				expect(tuple[2]).should_be('b')
+				expect(tuple[3]).should_be('c')
+			end
+		end
+		
+		describe["decoding array"] = function()
+			before = function()
+			 	decoder = Decoder:new(bytes_to_string { 131, 108, 0, 0, 0, 3, 109, 0, 0, 0, 1, 97, 109, 0, 0, 0, 1, 98, 109, 0, 0, 0, 1, 99, 106 })			
+			end
+			
+			it["should return encoded array"] = function()
+				local tuple = decoder:read_any()
+				expect(#tuple).should_be(3)
+				expect(tuple[1]).should_be('a')
+				expect(tuple[2]).should_be('b')
+				expect(tuple[3]).should_be('c')
+			end			
+		end
+		
+		describe["nexted array"] = function()
+			before = function()
+				decoder = Decoder:new(bytes_to_string { 131, 108, 0, 0, 0, 4, 109, 0, 0, 0, 1, 97, 109, 0, 0, 0, 1, 98, 109, 0, 0, 0, 1, 99, 108, 0, 0, 0, 2, 109, 0, 0, 0, 1, 102, 109, 0, 0, 0, 1, 111, 106, 106 })
+			end
+			
+			it["should return encoded array"] = function()
+				local tuple = decoder:read_any()
+				expect(#tuple).should_be(4)
+				expect(tuple[1]).should_be('a')
+				expect(tuple[2]).should_be('b')
+				expect(tuple[3]).should_be('c')
+				expect(tuple[4][1]).should_be('f')
+				expect(tuple[4][2]).should_be('o')
+			end			
+		end
+	end
+	
+end
