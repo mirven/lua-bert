0bd9e149c10b25143a6c68e969ef7e9bb0bc4ab0
Marcus Irven <marcus@marcusirven.com>
2009-11-23 00:42:28 -0600

    remove seeall from sym module

--- lib/bert/sym.lua
+++ lib/bert/sym.lua
@@ -1,3 +1,6 @@
+local setmetatable = setmetatable
+local getmetatable = getmetatable
+
 module('bert.sym', package.seeall)
 
 local Symbols = {}
@@ -14,41 +17,3 @@ function s(name)
 	Symbols[name] = Symbols[name] or create_sym(name)
 	return Symbols[name]
 end
-
--- Symbol = {}
--- -- Symbol.__index = Symbol
--- 
--- function Symbol:new(name)
--- 	Symbols[name] = Symbols[name] or setmetatable({ name = name }, Symbols)
--- 	return Symbols[name]
--- end
--- 
--- function sym(name)
--- 	Symbols[name] = Symbols[name] or create_sym(name)
--- 	return Symbols[name]
--- end
--- 
--- s = setmetatable({}, {
--- 	__index = function(_, name)
--- 		return sym(name)
--- 	end
--- });
--- 
--- -- should("blah blah bah", function()
--- -- end)
--- 
--- (function()
--- 	assert(type(sym("foo")) == "table")
--- 	assert(sym("foo") == sym("foo"))
--- 	assert(sym("foo") ~= sym("bar"))
--- end)();
--- 
--- (function()
--- 	assert(is_symbol(sym("foo")))
--- 	assert(not is_symbol("string"))
--- 	assert(not is_symbol({}))
--- end)();
--- 
--- (function()
--- 	assert(sym("blah") == s.blah)
--- end)();
